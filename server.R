1becc53652e6d7c2a0342d007a56ee71d8e378b9
EMFS <EMFS@users.noreply.github.com>
2015-06-21 11:50:38 -0300

    Changed functionality, added several inputs/ouputs

--- server.R
+++ server.R
@@ -3,14 +3,17 @@
 # 3. Some reactive output displayed as a result of server calculations
 
 library(shiny)
+library(lubridate)
 
 function(input, output) {
   
   # builds a reactive object that will be used by different other reactive functions
   reactiveNumber <- reactive({
-    input$num1
+    input$num
   })
   
   # make the output:
-  output$textOut <- renderText(paste(reactiveNumber(), '+', input$num2,'=', reactiveNumber() + input$num2))
+  output$textOut <- renderText({paste("Total number of fruits:",reactiveNumber(), '+', input$id1,'=', reactiveNumber() + input$id1)})
+  output$CheckBoxOut <- renderText({paste(input$id2)})
+  output$DeliveryOut <- renderPrint(input$DeliveryDate - Sys.Date())
 }
--- ui.R
+++ ui.R
@@ -3,20 +3,45 @@
 # 1. Some form of input (widget: textbox, radio button, checkbox, ...)
 # 4. You must also include enough documentation so that a novice user could use your application.
 
-# Idea: use the crimes DB from Chicago; do some basic analysis and visualization...
-# Problem => needs to import too much data! Better stick to the internal data sets.
-
 
 # Documentation: enough documentation on the shiny site for a user to get started using the application.
 
 library(shiny)
 
 fluidPage(
-  sliderInput(inputId = "num1", 
-              label = "Choose first operand", 
+  headerPanel("Simple use of multiple inputs"),
+  sidebarPanel(
+    h3('How to make this work?'),
+    p("There are several input objects below. Simply change any of their default values and watch the results! There won't be any fancy analysis, but the code can be reused to show diferent input/output methods, thus showing the capability of Shiny apps.")),
+  
+  # inputs
+  numericInput(inputId = 'id1', 
+               label = 'Number of bananas:',
+               value = 3,min = 0,max = 10,step = 1
+  ),
+  
+  sliderInput(inputId = "num", 
+              label = "Number of oranges:", 
               value = 5, min = 1, max = 10),
-  sliderInput(inputId = "num2", 
-              label = "Choose second operand", 
-              value = 5, min = 1, max = 10),
-  textOutput(outputId = "textOut")
+  
+  checkboxGroupInput(inputId = 'id2',label = "Fruit options",
+                     choices = c("Peeled" = "peeled",
+                                 "Mashed" = "mashed",
+                                 "Fresh" = "fresh",
+                                 "Imported" = "imported",
+                                 "Hand picked" = "hand picked"
+                     )
+  ),
+  dateInput(inputId = 'DeliveryDate',label = "Requested delivery date:"),
+  
+  # Outputs
+  mainPanel(
+    h3('Results Section'),
+    textOutput(outputId = "textOut"),
+    "Selected options:",
+    textOutput(outputId = "CheckBoxOut"),
+    p(),
+    'Expected delivery:',
+    textOutput(outputId = "DeliveryOut")
+  )
 )
